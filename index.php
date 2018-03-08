3490ef4a4b5f4992309de14c49a3ec73be42904c
Benjamin Martin <benji@rocketship.com>
2018-03-08 08:37:07 -0800

    this is my comment xx

--- index.php
+++ index.php
@@ -1,4 +1,8 @@
 <?php
+$con = new mysqli("127.0.0.1", "root", "password", "mydb");
+$message = $con->query("SELECT message FROM myTable")->fetch_object()->message;
+$con->close();
+echo "$message <br/>";
 echo "Hello From Sites Folder!";
 phpinfo();
 ?>
